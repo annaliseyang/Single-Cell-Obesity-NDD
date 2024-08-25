06fc7276ce7ba09fa79b3a7a7cb5702b05d78f8e
annaliseyang <anna_y@mit.edu>
2024-08-25 16:27:47 -0400

    deg_pathways

--- batch_cmd.sh
+++ batch_cmd.sh
@@ -1,11 +1,16 @@
 # input_dir="/home/anna_y/data/write/Subclass/"
 # pattern=/home/anna_y/data/write/*/*_small.h5ad
-pattern=/home/anna_y/data/results/deg_bmi_normalized/*/*/*_bmi.*
+# pattern=/home/anna_y/data/results/deg_bmi_normalized/*/*/*_bmi.*
+pattern=/home/anna_y/data/results/deg_bmi_normalized_v1_metascape/Class/*.zip
 
 for file in $pattern; do
   echo ""
   echo "File: $file"
-  new_filename=${file/"_bmi."/"."}
-  echo New filename: $new_filename
-  mv $file $new_filename
+  # new_filename=${file/"_bmi."/"."}
+  dir=${file/".zip"/""}
+  echo Dir: $dir
+  unzip $file -d $dir
+  rm $file
+  # echo New filename: $new_filename
+  # mv $file $new_filename
 done
--- deg_pathways.py
+++ deg_pathways.py
@@ -2,13 +2,15 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import os
 
-def plot_heatmap(celltype, pos_path, neg_path, out_path, all=True):
+def plot_heatmap(celltype, pos_path, neg_path, out_path, var='BestLogPInGroup', all=True):
     pos_results = pd.read_csv(pos_path)
     neg_results = pd.read_csv(neg_path)
 
-    pos_go, pos_logp = pos_results['GO'].tolist(), pos_results['LogP'].tolist()
-    neg_go, neg_logp = neg_results['GO'].tolist(), neg_results['LogP'].tolist()
+    pos_go, pos_description = pos_results['GO'].tolist(), pos_results['Description'].tolist()
+    neg_go, neg_description = neg_results['GO'].tolist(), neg_results['Description'].tolist()
+
     all_go = list(set(pos_go) | set(neg_go))
     common_go = list(set(pos_go) & set(neg_go))
 
@@ -18,41 +20,46 @@ def plot_heatmap(celltype, pos_path, neg_path, out_path, all=True):
     print(f"Common GO terms: {len(common_go)} {common_go}")
 
     go_terms = all_go if all else common_go
-    df = pd.DataFrame(0, index=go_terms, columns=['Positive', 'Negative'])
-    # df.loc[common_go, 'Positive'] = pos_results[pos_results['GO'].isin(common_go)]['LogP'].tolist()
-    # df.loc[common_go, 'Negative'] = neg_results[neg_results['GO'].isin(common_go)]['LogP'].tolist()
-    print(pos_results[pos_results['GO'].isin(go_terms)]['LogP'].tolist())
-    print(pos_results[pos_results['GO'].isin(go_terms)]['BestLogPInGroup'].tolist())
+    descriptions = [pos_description[pos_go.index(go)] if go in pos_go else neg_description[neg_go.index(go)] for go in go_terms]
+    labels = [f"{go}: {description}" for go, description in zip(go_terms, descriptions)]
 
-    # pos_col = -pd.Series(pos_results[pos_results['GO'].isin(go_terms)]['LogP'].tolist(), index=go_terms)
-    # neg_col = -pd.Series(neg_results[neg_results['GO'].isin(go_terms)]['LogP'].tolist(), index=go_terms)
-    pos_col = [-pos_results.at[pos_go.index(term), 'BestLogPInGroup'] if term in pos_go else 0 for term in go_terms]
-    neg_col = [-neg_results.at[neg_go.index(term), 'BestLogPInGroup'] if term in neg_go else 0 for term in go_terms]
+    df = pd.DataFrame(0, index=labels, columns=['Positive', 'Negative'])
+
+    pos_col = [-pos_results.at[pos_go.index(term), var] if term in pos_go else 0 for term in go_terms]
+    neg_col = [-neg_results.at[neg_go.index(term), var] if term in neg_go else 0 for term in go_terms]
 
     df['Positive'] = pos_col
     df['Negative'] = neg_col
+    # df['sum'] = df['Positive'] + df['Negative']
     df.sort_values(by=['Positive', 'Negative'], ascending=False, inplace=True)
     print(df)
 
     # create heatmap
     plt.figure(figsize=(12, 8))
-    plt.title(f"{celltype} GO Enrichment Heatmap")
-    sns.heatmap(df, annot=False, vmin=0, vmax=df.max().max())
+    plt.title(f"{celltype} Pathway Enrichment Heatmap ({var})")
+    sns.heatmap(df, cmap='Blues', annot=False, vmin=0, vmax=df.max().max())
 
     plt.xticks(rotation=90)
     plt.yticks(rotation=0)
-    plt.xlabel("Condition")
-    plt.ylabel("GO Terms")
+    plt.ylabel("Functional Ontology Term")
+    # plt.legend(loc='right', title=var)
     plt.savefig(out_path, bbox_inches='tight')
 
     print(f"Heatmap saved to {out_path}")
 
 
 if __name__ == "__main__":
-    pos_path = "/home/anna_y/data/results/deg_bmi_normalized_v1_metascape/Class/Exc_50k_positive/Enrichment_GO/_FINAL_GO.csv"
-    neg_path = "/home/anna_y/data/results/deg_bmi_normalized_v1_metascape/Class/Exc_50k_negative/Enrichment_GO/_FINAL_GO.csv"
-    out_path_common = "/home/anna_y/data/results/figures/deg_bmi_normalized_v1_GO/Class/Exc_50k_heatmap_common.png"
-    out_path_all = "/home/anna_y/data/results/figures/deg_bmi_normalized_v1_GO/Class/Exc_50k_heatmap_all.png"
+    celltypes = ['Exc_50k', 'Ast', 'Mic_Immune', 'OPC', 'Oli', 'Vasc_Epithelia', 'Inh']
+    # for dir in os.listdir("/home/anna_y/data/results/deg_bmi_normalized_v1_metascape/Class/"):
+    for celltype in celltypes:
+        print(celltype)
+        # continue
+        # celltype = dir.split('_')[0]
+        # sign = dir.split('_')[1]
+        pos_path = f"/home/anna_y/data/results/deg_bmi_normalized_v1_metascape/Class/{celltype}_positive/Enrichment_GO/_FINAL_GO.csv"
+        neg_path = f"/home/anna_y/data/results/deg_bmi_normalized_v1_metascape/Class/{celltype}_negative/Enrichment_GO/_FINAL_GO.csv"
+        out_path_common = f"/home/anna_y/data/results/figures/deg_bmi_normalized_v1_GO/Class/{celltype}_heatmap_common.png"
+        out_path_all = f"/home/anna_y/data/results/figures/deg_bmi_normalized_v1_GO/Class/{celltype}_heatmap_all.png"
 
-    plot_heatmap("Exc", pos_path, neg_path, out_path_common, all=False)
-    plot_heatmap("Exc", pos_path, neg_path, out_path_all, all=True)
+        plot_heatmap(celltype, pos_path, neg_path, out_path_common, all=False)
+        plot_heatmap(celltype, pos_path, neg_path, out_path_all, all=True)
